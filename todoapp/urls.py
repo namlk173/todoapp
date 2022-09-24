7f29fa61a048cb2b26b6a64890595f93faef40db
namlk173 <lekhacnam1703@gmail.com>
2022-09-24 17:40:27 +0700

    config static file

--- todoapp/urls.py
+++ todoapp/urls.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
 from django.urls import path, include
-
+from django.conf import settings
+from django.conf.urls.static import static
 from rest_framework import permissions
 from drf_yasg.views import get_schema_view
 from drf_yasg import openapi
@@ -22,10 +23,12 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('auth/', include("base.api.urls")),
     path('api/', include("todo.urls")), 
-    
+
     path("", schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
     path("redoc/", schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
     
 ]
+
+# urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
 
